d5b5e4c3419f887ea01c6297f4cb69681f270588
Jen <145509868+Jendunsmore@users.noreply.github.com>
2024-09-29 23:29:22 -0400

    started lab

---
+++ script.js
@@ -0,0 +1,41 @@
+// Part 1: Humble Beginnings
+
+// We will call the adventurer “Robin.”
+const adventurer = {
+name: "Robin",
+health: 10,
+inventory: ["sword", "potion", "artifact"]
+}
+
+// find a sword at adventurer.inventory[0].
+// create a loop that logs each item in Robin’s inventory.
+// Nested arrays are useful, but so are nested objects.
+
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat"
+    }
+    }
+
+
+
+
+
+
+// Part 2: Class Fantasy
+
+
+
+// Part 3: Class Features
+
+
+
+// Part 4: Class Uniforms
+
+
+
+// Part 5: Gather your Party
